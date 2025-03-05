3b7f00e682017e78540ed5fdb1e97fee128d40fb
salwa260706 <sukanasalsabillasalwa@gmail.com>
2025-03-05 11:35:22 +0700

    first

---
+++ pertemuan-1/list.py
@@ -0,0 +1,22 @@
+# Membuat Variabel
+# String Variabel
+namaMhs = "Hello World"
+print(namaMhs)
+print(type(namaMhs))
+# Variabel Integer
+umurMhs = 20
+print(umurMhs)
+print(type(umurMhs))
+# Variabel Float
+ipkMhs = 3.5
+print(ipkMhs)
+print(type(ipkMhs))
+# Variabel Bolean
+lulusMhs = 3.5>3.0
+print(lulusMhs)
+print(type(lulusMhs))
+
+# Membuat List 
+mylist = ["Hello World", 20, 3.5, True]
+print(f"ini isi List = {mylist}")
+print(type(mylist))
